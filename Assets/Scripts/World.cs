9c14bf2d1803ee6baa6dd71cd0ea220bc8438e81
Parker <parkerkshelton@gmail.com>
2018-12-18 16:28:41 -0500

    Starting to generate faces for blocks

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour {
+
+  public static World currentWorld;
+
+  public int chunkWidth = 20, chunkHeight = 20, seed = 0;
+
+  // Start is called before the first frame update
+  void Awake() {
+    currentWorld = this;
+
+    if(seed == 0) {
+      seed = Random.Range(0, int.MaxValue);
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+}
